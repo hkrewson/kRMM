05408912b6717d9ef7732dcf7b462b2b04af11b4
hkrewson <hkrewson@gmail.com>
2021-05-27 13:35:19 -0500

    Apache License
    Added the apache license

--- pi-update.sh
+++ pi-update.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# 
+# Copyright 2021 Hamlin Krewson
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# 	limitations under the License.
+#
 # We need a log file to keep track of installable software. 
 #	/var/log/apt/history.log keeps track of all installs and upgrades.
 #	We'll keep track of our list in a new /var/log/apt-updates.log file
@@ -22,6 +37,15 @@ apt update &> /dev/null
 
 #Gather a list of updates if available
 upgradable=($(apt list --upgradable))
+
+updatesLOG="/var/log/apt-updates.log"
+
+if [[ ! -e $updatesLOG ]]; then
+	touch $updatesLOG
+fi
+
+printf '%s '  $(date '+%a %d %b, %Y %T') >> $updatesLOG
+printf '\n' >> $updatesLOG
 
 #Set IFS to original
 IFS=$IFSOLD
