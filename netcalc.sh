63f65eff912b8cb4ff9634ea2dae175d6e12a444
hkrewson <hkrewson@gmail.com>
2021-06-15 19:02:52 -0500

    netcalc complete
    framework for the script is here. Need to test with classful addresses and random ips.

--- netcalc.sh
+++ netcalc.sh
@@ -7,23 +7,23 @@
 
 fSubnet() {
 	cider="$1"
-	echo $cider
+	#echo $cider
 	rcider=$(expr 32 - $cider)
-	echo $rcider
-	binstring=$(printf '1%.0s' {0..$cider};printf '0%.0s' {1..$rcider})
+	#echo $rcider
+	binstring=$(printf '1%.0s' {1..$cider};printf '0%.0s' {1..$rcider})
 	# In bash, printf doesn't allow passing a variable inside braces. Instead
 	#	pass it as a sequence like below.
 	#binstring=$(printf '1%.0s' $(seq 0 $cider);printf '0%.0s' $(seq 1 $rcider))
-	echo $binstring
+	#echo $binstring
 	subnetSTR=()
 	i=1
-	offset=1
+	offset=0
 	for i in 1 2 3 4; do
 		subnetSTR[$i]=${binstring:$offset:8}
 		i=$((i+1))
 		offset=$((offset+8))
 	done
-	echo $subnetSTR
+	#echo $subnetSTR
 }
 
 fIpToBin(){
@@ -41,11 +41,37 @@ fIpToBin(){
 	done
 }
 
-
-fBinMath(){
-	# Expects an equation of binary numbers like $binary1 + $binary2
-	1+=$(printf "%08d\n" $(bc <<< "ibase=2;obase=2;${2}"))
+fNetworkBin(){
+	# Convert the given IP to a binary representation of the Network ID
+	rem=${binstring:$CIDR:$rcider}
+	netLength=$(( ${#binstring} - ${#rem}))
+	ipbinflat=$(printf "%s" $BINIPINPUT)
+	networkIPBin=$(printf '%s%s' ${ipbinflat:0:$netLength}$rem)
+	ipbeginbin=$networkIPBin
+	broadcastbin=$(printf '%s' ${ipbinflat:0:$netLength};printf '1%.0s' {1..${#rem}})
+	ipavailbeginbin=$(printf '%s%s%s' ${ipbinflat:0:$netLength}${rem:0:${#rem}-1}1)
+	ipavailendbin=$(printf '%s' ${ipbinflat:0:$netLength};printf '1%.0s' {2..${#rem}};printf '0')
 }
+
+# fNetworkIP $binary ipname
+fBinToIP(){     
+	#convert the given binary numbers back to dot notation
+	offset=0
+	ipnum=''
+	for i in 1 2 3 4; do
+		octet=${2:$offset:8}
+		i=$((i+1))
+		offset=$((offset+8))
+		ipnum="${ipnum}$(printf "%d\n" $(bc <<< "ibase=2;$octet"))."
+	done
+	eval "$1=${ipnum%.}"
+}
+
+#fBinToDecMath(){ 
+#	# fBinToDecMath varname binarynumber
+#	eval "$1=$(printf "%d\n" $(bc <<< "ibase=2;$2"))" 
+#}
+
 
 fCIDR(){
 	# Test the input IP address for CIDR notation and split into vars
@@ -55,6 +81,24 @@ fCIDR(){
 	else
 		#IP may be Classful
 		IPINPUT=$1
+		
+		case $IPINPUT in
+			0.0.0.0) 
+				CIDR=1
+			;;
+			128.0.0.0)
+				CIDR=2
+			;;
+			192.0.0.0) 
+				CIDR=3
+			;;
+			224.0.0.0) 
+				CIDR=4
+			;;
+			240.0.0.0) 
+				CIDR=4
+			;;
+		esac
 	fi
 	# 
 	BINIPINPUT=()
@@ -65,3 +109,25 @@ fCIDR(){
 		i=$((i+1))
 	done
 }
+
+fCIDR 0.0.0.0
+fSubnet $CIDR
+fNetworkBin 
+availIPs=$((2 ** $rcider))
+hosts=$(($availIPs - 2))
+fBinToIP broadcast $broadcastbin
+fBinToIP networkIP $networkIPBin
+fBinToIP ipbeginIP $ipbeginbin
+fBinToIP ipavailbeginIP $ipavailbeginbin
+fBinToIP ipavailendIP $ipavailendbin
+fBinToIP subnetIP $binstring
+
+printf "%25s %s %s\n" "IP Input:" $IPINPUT
+printf "%25s %s\n %59s\n" "Network:" "$networkIP" "($networkIPBin)"
+printf "%25s %s\n %59s\n" "Subnet Mask:" $subnetIP "($binstring)"
+printf "%25s %s\n %59s\n" "Broadcast:" $broadcast "($broadcastbin)"
+printf "%25s %s\n %59s\n" "First IP:" $ipbeginIP "($ipbeginbin)"
+printf "%25s %s\n %59s\n" "First Available:" $ipavailbeginIP "($ipavailbeginbin)"
+printf "%25s %s\n %59s\n" "Last Available:" $ipavailendIP "($ipavailendbin)"
+printf "%25s %s\n" "Available IPs:" $availIPs
+printf "%25s %s\n" "Hosts:" "$hosts"
